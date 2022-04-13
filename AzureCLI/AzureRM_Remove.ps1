bac82ebe78da6a81af5d5dd14eec03a3fa3d332b
Tim Krebs <timkrebs@microsoft.com>
2022-04-13 13:00:42 +0200

    Azure scipts

---
+++ AzureCLI/AzureRM_Remove.ps1
@@ -0,0 +1,83 @@
+<#
+Copyright (c) 2022 Tim Krebs. All rights reserved.
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+#>
+
+function Uninstall-AllModules {
+    <#
+    .SYNOPSIS
+    Remove the AzureRM Module
+    .DESCRIPTION
+    This script uninstalls the AzureRM module with the correct version 
+    !!! You wil need Administrator access to execute the script in a scope
+
+    To find all installer AzureRM versions run:
+    "Get-InstalledModule -Name AzureRM -AllVersions"
+    .PARAMETER Name
+    Specifies the version which should get uninstalled
+    .EXAMPLE
+    Uninstall-AllModules -TargetModule AzureRM -Version <YourVersionHere> -Force
+    #>
+  param(
+    [Parameter(Mandatory=$true)]
+    [string]$TargetModule,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Version,
+
+    [switch]$Force,
+
+    [switch]$WhatIf
+  )
+
+  $AllModules = @()
+
+  'Creating list of dependencies...'
+  $target = Find-Module $TargetModule -RequiredVersion $version
+  $target.Dependencies | ForEach-Object {
+    if ($_.PSObject.Properties.Name -contains 'requiredVersion') {
+      $AllModules += New-Object -TypeName psobject -Property @{name=$_.name; version=$_.requiredVersion}
+    }
+    else { # Assume minimum version
+      # Minimum version actually reports the installed dependency
+      # which is used, not the actual "minimum dependency." Check to
+      # see if the requested version was installed as a dependency earlier.
+      $candidate = Get-InstalledModule $_.name -RequiredVersion $version -ErrorAction Ignore
+      if ($candidate) {
+        $AllModules += New-Object -TypeName psobject -Property @{name=$_.name; version=$version}
+      }
+      else {
+        $availableModules = Get-InstalledModule $_.name -AllVersions
+        Write-Warning ("Could not find uninstall candidate for {0}:{1} - module may require manual uninstall. Available versions are: {2}" -f $_.name,$version,($availableModules.Version -join ', '))
+      }
+    }
+  }
+  $AllModules += New-Object -TypeName psobject -Property @{name=$TargetModule; version=$Version}
+
+  foreach ($module in $AllModules) {
+    Write-Host ('Uninstalling {0} version {1}...' -f $module.name,$module.version)
+    try {
+      Uninstall-Module -Name $module.name -RequiredVersion $module.version -Force:$Force -ErrorAction Stop -WhatIf:$WhatIf
+    } catch {
+      Write-Host ("`t" + $_.Exception.Message)
+    }
+  }
+}
